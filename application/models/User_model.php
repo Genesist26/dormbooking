bad83fd7d949ec3b5b010b96ca3cf43685842366
Genesis <somsakwp8@outlook.com>
2018-12-22 19:50:32 +0700

    update model

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -19,4 +19,29 @@ class User_model extends CI_Model {
         return FALSE;
     }
 
+    public function add_new_user($username, $password)
+    {
+        $this->db->where('username', $username);
+        $query = $this->db->get('user');
+
+        if ($query->num_rows() > 0)
+        {
+            $row = $query->row();
+            $hash = $row->password;
+            if ($password == $hash) {
+                return $row->username;
+            }
+        }
+        return FALSE;
+    }
+
+//    public function insert_data($username, $password, $question, $answer)
+//    {
+//        return $this->db->insert('user', $username, $password, $question, $answer);
+//    }
+    public function insert_data($data)
+    {
+        return $this->db->insert('user', $data);
+    }
+
 }
