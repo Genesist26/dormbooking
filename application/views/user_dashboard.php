08eed238714a23a39ffe0d94d1ba291de9150309
praew <preaw_10120@hotmail.com>
2018-12-23 12:12:02 +0700

    add dashboard

--- application/views/user_dashboard.php
+++ application/views/user_dashboard.php
@@ -1,11 +1,44 @@
 <!--<link rel="stylesheet" href="public/css/homepate.css">-->
+<link rel="stylesheet" href="public/css/user_dashboard.css">
+<link rel="stylesheet" href="public/css/bootstrap.css">
 <div class="site-main">
-    <p align="center">user_dashboard_view.php</p>
-    <h2>Dashboard</h2>
-    <a href="<?= base_url('Repair');?>">Repair</a><br>
-    <a href="<?= base_url('Track');?>">Track</a><br>
-    <a href="<?= base_url('Information');?>">Information</a><br>
-    <a href="<?= base_url('Booking');?>">Booking/Move</a><br>
-    <a href="<?= base_url('Contact');?>">Contact Us</a><br>
-    <a href="<?= base_url('Setting');?>">Setting</a><br>
+    <!--<p align="center">user_dashboard_view.php</p>-->
+
+    <div class="h"> <h2>Dashboard</h2></div>
+    <!--<a class="btn btn-large" href="#"><i class="fa fa-wrench fa-2x "></i> Star</a>-->
+
+    <div class="icon1">
+        <i class=" fa fa-wrench fa-5x"></i>
+        <a href="#"><span class="pull-left"><a href="<?= base_url('service'); ?>">Service</a></span>
+        </a>
+    </div>
+    <div class="icon2">
+        <i class=" fa fa-search  fa-5x"></i>
+        <a href="#"><span class="pull-left"><a href="<?= base_url('Track'); ?>">Track</a></span>
+        </a>
+    </div>
+    <div class="icon3">
+        <i class=" fa fa-exclamation-circle   fa-5x"></i>
+        <a href="#"><span class="pull-left"><a href="<?= base_url('Booking'); ?>">Booking/Move</a><br><br></span>
+        </a>
+    </div>
+
+
+    <div class="icon4">
+        <i class=" fa fa-forward fa-5x"></i>
+        <a href="#"><span class="pull-left"><a href="<?= base_url('Booking'); ?>">Booking/Move</a></span>
+        </a>
+    </div>
+    <div class="icon5">
+        <i class=" fa fa-envelope fa-5x"></i>
+        <a href="#"><span class="pull-left"><a href="<?= base_url('Contact'); ?>">Contact Us</a></span>
+        </a>
+    </div>
+    <div class="icon6">
+        <i class=" fa fa-gears fa-5x"></i>
+        <a href="#"><span class="pull-left">  <a href="<?= base_url('Setting'); ?>">Setting</a></span>
+        </a>
+    </div>
+
+
 </div>
