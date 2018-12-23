f0f1c80292d2f9b92816b5f1a4e0ff3666b0d816
praew <preaw_10120@hotmail.com>
2018-12-24 02:53:49 +0700

    edit table repair

--- application/views/manage_repair.php
+++ application/views/manage_repair.php
@@ -3,20 +3,47 @@
     <h2 align="center">Manage Repair</h2>
     <p>Resize the browser window to see the effect. When the screen is less than 600px wide, make the two columns stack
         on top of each other instead of next to each other.</p>
+    <style>
+        table, th, td {
+            text-align: center;
+            table-layout: fixed;
+            border-collapse: collapse;
+            border: 1px solid black;
+            padding: 5px;
+        }
+        th{
+            background-color: #9fcdff;
+        }
+    </style>
+<?php
+    echo " <table>
+        <tr>
+            <th>ID</th>
+            <th>username</th>
+            <th>subject</th>
+            <th>room</th>
+            <th>object</th>
+            <th>detail</th>
+            <th>status</th>
+            <th>timestamp</th>
+        </tr>";?>
+        <?php foreach ($queue as $item):?>
 
-    id, username, subject, room, object, detail, status timestamp<br>
-    <?php if($queue) : ?>
-        <?php foreach ($queue as $item) : ?>
             <?php
             $queue_id = $item['id'];
             ?>
-            <?php foreach ($item as $key => $value) : ?>
-                <?php echo $value."\t"; ?>
-            <?php endforeach; ?>
-            <a href="<?= base_url("repair/update_queue_status/$queue_id"); ?>">complete</a><br>
-        <?php endforeach; ?>
+            <tr>
+                <td><?= $item['id'] ?></td>
+                <td><?= $item['username'] ?></td>
+                <td><?= $item['subject'] ?></td>
+                <td><?= $item['room'] ?></td>
+                <td><?= $item['object'] ?></td>
+                <td><?= $item['detail'] ?></td>
+                <td><?= $item['status'] ?></td>
+                <td><?= $item['timestamp'] ?></td>
+                <td><a href="<?= base_url("repair/update_queue_status/$queue_id"); ?>">complete</a><br></td>
+            </tr>
+        <?php endforeach;?>
 
-    <?php endif; ?>
-
-</div>
+    </table>
 </div>
